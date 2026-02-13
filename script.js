4d25265ba7b02f7189d10308cb694562fdba02a2
Wjimenezg <139941709+Wjimenezg@users.noreply.github.com>
2026-02-13 13:40:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const noBtn = document.getElementById("btn-random");
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+let scale = 1;
 
 if(noBtn){
-    noBtn.addEventListener("mouseover", function () {
-        const x = Math.random() * (window.innerWidth - 100);
-        const y = Math.random() * (window.innerHeight - 50);
 
-        noBtn.style.position = "absolute";
-        noBtn.style.left = x + "px";
-        noBtn.style.top = y + "px";
+    // Para celular (click)
+    noBtn.addEventListener("click", function(e){
+        e.preventDefault();
+        crecerBoton();
     });
+
+    // Para computadora (cuando pasa el mouse)
+    noBtn.addEventListener("mouseover", function(){
+        moverBoton();
+    });
+
+}
+
+function crecerBoton(){
+    scale += 0.3;
+    yesBtn.style.transform = `scale(${scale})`;
+}
+
+function moverBoton(){
+    const x = Math.random() * (window.innerWidth - 100);
+    const y = Math.random() * (window.innerHeight - 50);
+
+    noBtn.style.position = "absolute";
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
 }
